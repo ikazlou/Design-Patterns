56fc563a6f52201b166c2d057c4d5dd9f967d001
ikazlou <ikazlou@intelliflo.com>
2022-06-10 11:24:51 +0200

    [Refactoring-GuRu] Astract-factory

---
+++ src/AbstractFactory/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        //https://refactoring.guru/ru/design-patterns/abstract-factory/csharp/example#lang-features
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
