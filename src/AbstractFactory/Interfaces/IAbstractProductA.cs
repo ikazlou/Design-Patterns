afe030cbfa5118e1139999568dd702ecafadc5d8
LAPTOP-PV0JL093\Hamidalion <ikazlouwork@gmail.com>
2024-01-23 20:14:08 +0100

    New init

--- src/AbstractFactory/Interfaces/IAbstractProductA.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AbstractFactory.Interfaces
-{
-    // Каждый отдельный продукт семейства продуктов должен иметь базовый
-    // интерфейс. Все вариации продукта должны реализовывать этот интерфейс.
-    public interface IAbstractProductA
-    {
-        string UsefulFunctionA();
-    }
-}
--- src/ClassicFactory/ClassicFactory/PointFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClassicFactory
-{
-    public class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y) => new Point(x, y); // Фабричный метод
-        public static Point NewPolarPoint(double rho, double theta) => new Point(rho * Math.Cos(theta), rho * Math.Sin(theta)); // Фабричный метод
-
-    }
-}
