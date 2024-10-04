56b4ba8b65937d5cc0ed2e95a9c8d9ce2bbce23f
GODELTECH\i.kazlou <i.kazlou@godeltech.com>
2024-10-04 15:29:22 +0200

    feat: remind and add comment

--- src/Builder/Builder_Multifunctional/Builder/PersonBuilder.cs
+++ src/Builder/Builder_Multifunctional/Builder/PersonBuilder.cs
@@ -14,10 +14,17 @@ public class PersonBuilder // Facade
         Person = person;
     }
 
-    public PersonAddressBuilder Lives => new(person: Person);
+    public PersonAddressBuilder Lives => new(Person);
 
     public PersonJobBuilder Works => new(Person);
 
+    /// <summary>
+    ///     implicit определяет неявное преобразование. Это значит, что когда требуется объект класса Person,
+    ///     компилятор автоматически преобразует объект PersonBuilder в объект Person,
+    ///     не требуя явного вызова преобразования (например, вызова метода или использования оператора преобразования).
+    /// </summary>
+    /// <param name="personBuilder"></param>
+    /// <returns>Person -> PersonBuilder</returns>
     public static implicit operator Person(PersonBuilder personBuilder)
     {
         return personBuilder.Person;
